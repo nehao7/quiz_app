62b01f1a71be5c7bbf545291ef6abb8f22af35be
nehao7 <157192328+nehao7@users.noreply.github.com>
2025-06-05 18:06:14 +0530

    webview replaced issue resolved

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,143 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
 import 'package:quiz_app/WebPlusNotification.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() async {
+import 'NotificationScreen.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+  final notif = NotificationItem(
+        title: message.notification?.title ?? 'No Title',
+        body: message.notification?.body ?? 'No Body',
+        time: DateFormat('dd-MMM-yyyy hh:mm a').format(DateTime.now()),
+      );
+      await FirebaseFirestore.instance.collection('notifications').add(notif.toMap());
+
+  // await DatabaseHelper().insertNotification(message.notification!.title!, message.notification!.body!);
+
+  // if (message.data.isNotEmpty || message.notification != null) {
+  //   flutterLocalNotificationsPlugin.show(
+  //     message.hashCode,
+  //     message.notification?.title ?? message.data['title'] ?? 'Background Notification',
+  //     message.notification?.body ?? message.data['body'] ?? 'You have a new message!',
+  //     const NotificationDetails(
+  //       android: AndroidNotificationDetails(
+  //         'high_importance_channel',
+  //         'High Importance Notifications',
+  //         channelDescription: 'This channel is used for important notifications.',
+  //         importance: Importance.max,
+  //         priority: Priority.high,
+  //       ),
+  //       iOS: DarwinNotificationDetails(
+  //         presentAlert: true,
+  //         presentBadge: true,
+  //         presentSound: true,
+  //       ),
+  //     ),
+  //     payload: message.data['click_action'],
+  //   );
+  // }
+}
+Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final DarwinInitializationSettings initializationSettingsDarwin =
+  DarwinInitializationSettings(
+
+    // onDidReceiveLocalNotification: (id, title, body, payload) async {},
+  );
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse notificationResponse) async {
+    print('Notification tapped! Payload: ${notificationResponse.payload}');
+  },
+  );
+
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.max,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await _requestNotificationPermissions();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  final message = await FirebaseMessaging.instance.getInitialMessage();
+  runApp(const MyApp());
 
-  // Get the notification that opened the app (if any)
-  RemoteMessage? initialMessage =
-  await FirebaseMessaging.instance.getInitialMessage();
-
-  runApp(MyApp(initialMessage: initialMessage));
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // final message = await FirebaseMessaging.instance.getInitialMessage();
+  //
+  // // Get the notification that opened the app (if any)
+  // RemoteMessage? initialMessage =
+  // await FirebaseMessaging.instance.getInitialMessage();
+  //
+  // runApp(MyApp(initialMessage: initialMessage));
 }
+
+Future<void> _requestNotificationPermissions() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission for notifications.');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission (iOS only).');
+  } else {
+    print('User declined or has not yet granted permission for notifications.');
+  }
+}
+
 //   runApp(const MyApp());
 // }
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print('Background message: ${message.messageId}');
-
-
-  final title = message.notification?.title ?? 'No Title';
-  final body = message.notification?.body ?? 'No Body';
-  final time = DateTime.now().toString(); // Optional: format if needed
-
-  await FirebaseFirestore.instance.collection('notifications').add({
-    'title': title,
-    'body': body,
-    'time': time,
-  });
-}
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   await Firebase.initializeApp();
+//   print('Background message: ${message.messageId}');
+//
+//
+//   final title = message.notification?.title ?? 'No Title';
+//   final body = message.notification?.body ?? 'No Body';
+//   final time = DateTime.now().toString(); // Optional: format if needed
+//
+//   await FirebaseFirestore.instance.collection('notifications').add({
+//     'title': title,
+//     'body': body,
+//     'time': time,
+//   });
+// }
 class MyApp extends StatelessWidget {
   const MyApp({super.key, RemoteMessage? initialMessage});
 
